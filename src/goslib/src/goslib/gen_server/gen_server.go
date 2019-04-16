91f3b820f6bcfe50735a0d7896813d36651eee64
savin <mafei.198@gmail.com>
2019-04-16 17:05:33 +0800

    refactor: 1.use sync.Map to replace cmap; 2.add pool for gen_server request and response object

--- src/goslib/src/goslib/gen_server/gen_server.go
+++ src/goslib/src/goslib/gen_server/gen_server.go
@@ -3,17 +3,17 @@ package gen_server
 import (
 	"errors"
 	"fmt"
-	"goslib/cmap"
 	"goslib/logger"
+	"sync"
 )
 
 const SIGN_STOP = 1
 
-var ServerRegisterMap = cmap.NewCMap()
+var ServerRegisterMap = sync.Map{}
 
 const (
-	CALL = iota
-	CAST
+	CALL byte = 0
+	CAST byte = 1
 )
 
 type Packet struct {
@@ -24,19 +24,26 @@ type Packet struct {
 type SignPacket struct {
 	signal           int
 	reason           string
-	response_channel chan *ResponsePacket
+	response_channel chan *Response
 }
 
-type ResponsePacket struct {
+type Response struct {
 	result interface{}
 	err    error
+}
+
+type Request struct {
+	category byte
+	//handler string
+	resultChan chan *Response
+	args []interface{}
 }
 
 type GenServer struct {
 	name        string
 	callback    GenServerBehavior
-	msgChannel  chan []interface{}
-	signChannel chan SignPacket
+	msgChannel  chan *Request
+	signChannel chan *SignPacket
 }
 
 type GenServerBehavior interface {
@@ -46,21 +53,34 @@ type GenServerBehavior interface {
 	Terminate(reason string) (err error)
 }
 
+var requestPool = sync.Pool{
+	New: func() interface{} {
+		return &Request{
+			resultChan: make(chan *Response),
+		}
+	},
+}
+
+var responsePool = sync.Pool{
+	New: func() interface{} {
+		return &Response{}
+	},
+}
+
 func setGenServer(name string, instance *GenServer) {
-	ServerRegisterMap.Set(name, instance)
+	ServerRegisterMap.Store(name, instance)
 }
 
 func GetGenServer(name string) (*GenServer, bool) {
-	v := ServerRegisterMap.Get(name)
-	if v == nil {
-		return nil, false
-	} else {
-		return v.(*GenServer), true
+	if v, ok := ServerRegisterMap.Load(name); ok {
+		return v.(*GenServer), ok
 	}
+	return nil, false
 }
 
 func Exists(name string) bool {
-	return ServerRegisterMap.Check(name)
+	_, ok := ServerRegisterMap.Load(name)
+	return ok
 }
 
 func delGenServer(name string) {
@@ -70,8 +90,8 @@ func delGenServer(name string) {
 func Start(server_name string, module GenServerBehavior, args ...interface{}) (gen_server *GenServer, err error) {
 	gen_server, exists := GetGenServer(server_name)
 	if !exists {
-		msgChannel := make(chan []interface{}, 1024)
-		signChannel := make(chan SignPacket)
+		msgChannel := make(chan *Request, 1024)
+		signChannel := make(chan *SignPacket)
 
 		gen_server = &GenServer{
 			name:        server_name,
@@ -95,8 +115,8 @@ func Start(server_name string, module GenServerBehavior, args ...interface{}) (g
 
 func Stop(server_name, reason string) error {
 	if gen_server, exists := GetGenServer(server_name); exists {
-		response_channel := make(chan *ResponsePacket)
-		gen_server.signChannel <- SignPacket{
+		response_channel := make(chan *Response)
+		gen_server.signChannel <- &SignPacket{
 			signal:           SIGN_STOP,
 			reason:           reason,
 			response_channel: response_channel,
@@ -111,14 +131,7 @@ func Stop(server_name, reason string) error {
 
 func Call(server_name string, args ...interface{}) (interface{}, error) {
 	if gen_server, exists := GetGenServer(server_name); exists {
-		response_channel := make(chan *ResponsePacket)
-		defer func() {
-			close(response_channel)
-		}()
-		args = append(args, response_channel, CALL)
-		gen_server.msgChannel <- args
-		packet := <-response_channel
-		return packet.result, packet.err
+		return gen_server.Call(args...)
 	} else {
 		errMsg := fmt.Sprintf("GenServer call failed: %s %s", server_name, " server not found!")
 		logger.ERR(errMsg)
@@ -128,9 +141,38 @@ func Call(server_name string, args ...interface{}) (interface{}, error) {
 
 func Cast(server_name string, args ...interface{}) {
 	if gen_server, exists := GetGenServer(server_name); exists {
-		args = append(args, CAST)
-		gen_server.msgChannel <- args
+		gen_server.Cast(args)
 	}
+}
+
+func (self *GenServer)Call(args ...interface{}) (interface{}, error) {
+	request := requestPool.Get().(*Request)
+	defer func() {
+		requestPool.Put(request)
+	}()
+	request.category = CALL
+	//request.handler = handler
+	request.args = args
+
+	self.msgChannel <- request
+
+	packet := <-request.resultChan
+	result := packet.result
+	err := packet.err
+
+	responsePool.Put(packet)
+
+	return result, err
+}
+
+func (self *GenServer)Cast(args ...interface{}) {
+	request := requestPool.Get().(*Request)
+	defer func() {
+		requestPool.Put(request)
+	}()
+	request.category = CAST
+	request.args = args
+	self.msgChannel <- request
 }
 
 func loop(gen_server *GenServer) {
@@ -138,37 +180,37 @@ func loop(gen_server *GenServer) {
 		terminate(gen_server)
 	}()
 
+	var sign_packet *SignPacket
+	var req *Request
+	var ok bool
 	for {
 		select {
-		case args, ok := <-gen_server.msgChannel:
+		case req, ok = <-gen_server.msgChannel:
 			if ok {
-				size := len(args)
-				callType := args[size-1].(int)
-				switch callType {
+				switch req.category {
 				case CALL:
-					response_channel := args[size-2]
-					result, err := gen_server.callback.HandleCall(args[0 : size-2])
-					response_channel.(chan *ResponsePacket) <- &ResponsePacket{
-						result: result,
-						err:    err,
-					}
+					result, err := gen_server.callback.HandleCall(req.args)
+					resp := responsePool.Get().(*Response)
+					resp.result = result
+					resp.err = err
+					req.resultChan <- resp
 					break
 				case CAST:
-					gen_server.callback.HandleCast(args)
+					gen_server.callback.HandleCast(req.args)
 					break
 				}
 			}
-		case sign_packet, ok := <-gen_server.signChannel:
+		case sign_packet, ok = <-gen_server.signChannel:
 			if ok {
 				switch sign_packet.signal {
 				case SIGN_STOP:
 					if err := gen_server.callback.Terminate(sign_packet.reason); err != nil {
 						logger.ERR("GenServer stop failed: ", err)
-						sign_packet.response_channel <- &ResponsePacket{
+						sign_packet.response_channel <- &Response{
 							err: err,
 						}
 					} else {
-						sign_packet.response_channel <- &ResponsePacket{
+						sign_packet.response_channel <- &Response{
 							err: nil,
 						}
 						return
