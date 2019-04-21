041506d259279864624e3d6631b5a99172c3c19a
savin <mafei.198@gmail.com>
2019-04-21 19:20:02 +0800

    refactor: delete unused files

--- generator/db/migrate/20180502060707_create_schema_persistance.rb
+++
@@ -1,9 +0,0 @@
-class CreateSchemaPersistance < ActiveRecord::Migration[5.2]
-  def change
-    create_table :schema_persistances, id: false do |t|
-      t.string :uuid
-      t.integer :version
-    end
-    add_index :schema_persistances, :uuid, unique: true
-  end
-end
